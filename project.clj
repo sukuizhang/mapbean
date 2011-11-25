b3d5e10cd3d8df87b983ca430ffd64bcf8d548c9
sukuizhang <skz@skzdeMacBook-Pro.local>
2011-11-25 14:26:21 +0800

    add function name translate

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mapbean "0.0.1"
+(defproject mapbean "0.0.2"
   :description "map bean to clojure functions"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.springframework/spring "2.5.6"]])
