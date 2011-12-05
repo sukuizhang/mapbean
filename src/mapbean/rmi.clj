b234be5cb5d8e016b6832ddc6d44468ceb0668d9
sukuizhang <skz@skzdeMacBook-Pro.local>
2011-12-05 17:56:48 +0800

    add comment for rmi.clj

--- src/mapbean/rmi.clj
+++ src/mapbean/rmi.clj
@@ -6,26 +6,37 @@
   (:import [org.springframework.remoting.rmi RmiProxyFactoryBean]))
 
 (defn- get-service0
-     [service-interface service-url]
-     (try 
-       (let [rmi (RmiProxyFactoryBean.)]
-         (doto rmi
-           (.setServiceInterface service-interface)
-           (.setServiceUrl service-url)
-           (.setLookupStubOnStartup true)
-           (.setRefreshStubOnConnectFailure true)
-           (.afterPropertiesSet))
-         (.getObject rmi))
-       (catch Exception e nil)))
+  "get rmi service according to service interface and service url, if any exception occur, nil is returned."
+  [service-interface service-url]
+  (try 
+    (let [rmi (RmiProxyFactoryBean.)]
+      (doto rmi
+        (.setServiceInterface service-interface)
+        (.setServiceUrl service-url)
+        (.setLookupStubOnStartup true)
+        (.setRefreshStubOnConnectFailure true)
+        (.afterPropertiesSet))
+      (.getObject rmi))
+    (catch Exception e nil)))
 
 (defn url
-  [service-type rmi-url name]
+  "build a rmi service url, if input service name is nil, use default rule to create service name.
+   default rule is like below:
+       abc.xyz.IUserService --> userService
+       abc.xyz.ActionBuilder --> actionBuilder
+  service-interface: service interface
+  rmi-url: like 192.168.0.137:3257
+  service-name: service name"
+  [service-interface rmi-url service-name]
   (str "rmi://" rmi-url "/" 
-       (or name (let [s-name (.getSimpleName service-type)
+       (or service-name (let [s-name (.getSimpleName service-interface)
                       s-name (if (.startsWith s-name "I") (.substring s-name 1) s-name)]
                   (str (Character/toLowerCase (.charAt s-name 0)) (.substring s-name 1))))))
 
-(defn create-service-factory [urls]
+(defn create-service-factory
+  "build a service factory using an url list, an url like 192.168.0.137:3257
+  the service factory take a service interface and an option service name, and return a service, it try all the url in urls one by one to get this service, and return one when first time success."
+  [urls]
   (memoize (fn [service-interface & [service-name]]
              (->> (if (fn? urls) (urls) urls)                  
                   (map #(get-service0 service-interface (url service-interface % service-name)))
@@ -33,6 +44,11 @@
                   (first)))))
 
 (defn- delay-handler
+  "create an java.lang.reflect.InvocationHandler used to delay get service action, use it, we create a proxy for this service-type, and do get when first invoke on this proxy.
+ factory: a service factory underlying create service.
+ service-type: service interface
+ service-name: input service name, if it is nil, use default rule to create
+ ops: options."
   [factory service-type service-name ops]
   (let [real (atom nil)
         service-str (str "[service type:"
@@ -66,6 +82,8 @@
                              (str "can not get rmi service" service-str)))))))))))
 
 (defn- wrap-delay
+  "create a wrapped service factory,it create a proxy for special service type.
+  when first invoke on this proxy, it create real service using the underlying service factory, and delegate the invoke to the real service."
   [factory & [ops]]
   (fn [service-type service-name]
     (let [handler (delay-handler factory service-type service-name ops)]
@@ -74,6 +92,7 @@
                                                 handler))))
 
 (defn map-service
+  "map java service methods to clojure functions and put it into special namespace."
   [service-type urls & [ops]]
   (let [service-name (:service-name ops)
         factory (-> (create-service-factory urls)
@@ -84,6 +103,7 @@
     (mapbean/map-bean service ns interceptors)))
 
 (defn map-services
+  "map many java services methods to clojure functions and put it into special namespace."
   [service-types urls & [ops]]
   (doseq [service-type service-types]
     (map-service service-type urls ops)))
